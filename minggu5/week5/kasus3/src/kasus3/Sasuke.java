ec99b36c62fc4c24de123dfb73ae85fc1c932eaa
Abdul Rangga Nopa Irhaz Putra <91001682+abdulrangganip3@users.noreply.github.com>
2021-10-15 17:18:05 +0700

    Add files via upload

---
+++ minggu5/week5/kasus3/src/kasus3/Sasuke.java
@@ -0,0 +1,9 @@
+package kasus3;
+public class Sasuke extends itachi {
+    String Dojutsu = "Sharingan";
+	
+	void printDojutsu() {
+		super.printDojutsu();
+		System.out.println(this.Dojutsu);
+	}
+}
